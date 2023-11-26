f0c61af4399feeb0a3a11f142c431799d7cf320a
wuguishifu <49931047+wuguishifu@users.noreply.github.com>
2023-11-25 20:08:35 -0800

    added some cloud functions for auto generated user data on signup and sending and accepting server invitations

--- backend/src/types/environment.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NODE_ENV: 'development' | 'production' | undefined;
-        }
-    }
-}
-
-export default global;
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,19 +1,98 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as logger from "firebase-functions/logger";
+import { onRequest } from 'firebase-functions/v2/https';
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const app = admin.initializeApp(functions.config().firebase);
+const firestore = app.firestore();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const onUserCreate = functions.auth.user().onCreate(async (user) => {
+    const { uid, email } = user;
+    const userRef = firestore.doc(`users/${uid}`);
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+        await userRef.set({ email, invitations: [], joined: [] });
+        logger.info(`User with email ${email} created.`);
+    }
+});
+
+export const onUserDelete = functions.auth.user().onDelete(async (user) => {
+    const { uid, email } = user;
+    const userRef = firestore.doc(`users/${uid}`);
+    const snapshot = await userRef.get();
+
+    if (snapshot.exists) {
+        await userRef.delete();
+        logger.info(`User with email ${email} deleted.`);
+    }
+});
+
+export const sendInvitation = onRequest({ timeoutSeconds: 5 }, async (req, res) => {
+    const { user_id, server_id, other_id } = req.body;
+    if (!user_id || !server_id || !other_id || typeof user_id !== 'string' || typeof server_id !== 'string' || typeof other_id !== 'string') {
+        res.status(400).send('Invalid request body. Must include user_id, server_id, and other_id.');
+        return;
+    }
+
+    logger.info(`Sending invitation from user ${user_id} to user ${other_id} for server ${server_id}.`);
+
+    const serverRef = firestore.doc(`servers/${server_id}`);
+    const otherRef = firestore.doc(`users/${other_id}`);
+    const [serverSnapshot, otherSnapshot] = await Promise.all([serverRef.get(), otherRef.get()]);
+
+    if (!serverSnapshot.exists) {
+        return void res.status(404).send(`Server ${server_id} not found.`);
+    }
+
+    if (!otherSnapshot.exists) {
+        return void res.status(404).send(`User ${other_id} not found.`);
+    }
+
+    if (!(serverSnapshot.data()?.owner === user_id)) {
+        return void res.status(404).send(`Server ${server_id} not found.`);
+    }
+
+    if (otherSnapshot.data()?.joined?.includes(server_id)) {
+        return void res.status(409).send(`User ${other_id} is already a member of server ${server_id}.`);
+    }
+
+    await otherRef.update({ invitations: admin.firestore.FieldValue.arrayUnion(server_id) });
+    res.status(200).send('Invitation sent.');
+});
+
+export const acceptInvitation = onRequest({ timeoutSeconds: 5 }, async (req, res) => {
+    const { user_id, server_id } = req.body;
+    if (!user_id || !server_id || typeof user_id !== 'string' || typeof server_id !== 'string') {
+        res.status(400).send('Invalid request body. Must include user_id and server_id.');
+        return;
+    }
+
+    logger.info(`Accepting invitation from user ${user_id} for server ${server_id}.`);
+
+    const serverRef = firestore.doc(`servers/${server_id}`);
+    const userRef = firestore.doc(`users/${user_id}`);
+    const [serverSnapshot, userSnapshot] = await Promise.all([serverRef.get(), userRef.get()]);
+
+    if (!serverSnapshot.exists) {
+        return void res.status(404).send(`Server ${server_id} not found.`);
+    }
+
+    if (!userSnapshot.exists) {
+        return void res.status(404).send(`User ${user_id} not found.`);
+    }
+
+    if (!(userSnapshot.data()?.invitations?.includes(server_id))) {
+        return void res.status(409).send(`User ${user_id} has not been invited to server ${server_id}.`);
+    }
+
+    await Promise.all([
+        serverRef.update({ members: admin.firestore.FieldValue.arrayUnion(user_id) }),
+        userRef.update({
+            invitations: admin.firestore.FieldValue.arrayRemove(server_id),
+            joined: admin.firestore.FieldValue.arrayUnion(server_id)
+        })
+    ]);
+
+    res.status(200).send('Invitation accepted.');
+});
